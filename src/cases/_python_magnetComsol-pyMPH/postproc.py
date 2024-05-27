1c0924a2f4b6d8e0f528c49cd36a0e8e0d22d4bb
Jeremie Muzet <jeremie.muz@gmail.com>
2024-05-27 13:41:46 +0200

    add 3D + tracking (docs skip)

--- src/cases/_python_magnetComsol-pyMPH/postproc.py
+++ src/cases/_python_magnetComsol-pyMPH/postproc.py
@@ -1,10 +1,11 @@
 import numpy as np
 from typing import List
 from methods import get_markers, create_group, json_get
+from rich.progress import track
 
 
 def postprocessing(
-    model, equations: List[str], data: dict, selection_import: dict, args
+    model, equations: List[str], data: dict, selection_import: dict, dim: int, args
 ):
     ### Create the post-proc
     print("Info    : Creating Post-Processing...")
@@ -23,86 +24,96 @@ def postprocessing(
     ### Browse equations and create corresponding post processing
     for eq in equations:
         if eq != "elastic2":
-            export_method[eq](plots, equations, args)
-            if json_get(data, "PostProcess", eq, "Measures", "Statistics"):
+            export_method[eq](plots, dim, equations, args)
+            stats = json_get(data, "PostProcess", eq, "Measures", "Statistics")
+            if stats:
+                print(f"Info    :    Statistics {eq}")
                 tab.create("Table", name=f"{eq}_values")
-                stat_method(eq, model, data, selection_import, args)
+                stat_method(eq, model, stats, selection_import, dim, args)
 
     print("Info    : Done creating Post-Processing")
 
 
-def create_export_magnetic(plots, equations: str, args):
-    print("Info    :    Export Magnetic")
+def create_export_magnetic(plots, dim: int, equations: str, args):
+    print("Info    :    Export magnetic")
 
-    create_export(plots, "Magnetic field", "normB", "mf.normB")
+    create_export(plots, dim, "Magnetic field", "normB", "mf.normB")
     if args.axis:
-        create_export(plots, "Magnetic potential", "Aphi", "mf.Aphi")
-        create_export(plots, "Current Density", "Jphi", "u")
+        create_export(plots, dim, "Magnetic potential", "Aphi", "mf.Aphi")
+        create_export(plots, dim, "Current Density", "Jphi", "u")
+    elif dim == 2:
+        create_export(plots, dim, "Magnetic potential", "Az", "mf.Az")
+        create_export(plots, dim, "Current Density", "Jz", "u")
     else:
-        create_export(plots, "Magnetic potential", "Az", "mf.Az")
-        create_export(plots, "Current Density", "Jz", "u")
+        create_export(plots, dim, "Magnetic potential", "A", "mf.normA")
+        create_export(plots, dim, "Current Density", "J", "mf.normJ")
 
 
-def create_export_heat(plots, equations: str, args):
-    print("Info    :    Export Heat")
+def create_export_heat(plots, dim: int, equations: str, args):
+    print("Info    :    Export heat")
 
-    create_export(plots, "Temperature", "T", "T")
-    create_export(plots, "Heat Source", "Q", "ht.Q")
-
-
-def create_export_electric(plots, equations: str, args):
-    print("Info    :    Export Electric")
-
-    create_export(plots, "Electric Potential", "V", "V")
-    create_export(plots, "Electric Field", "E", "ec.normE")
+    create_export(plots, dim, "Temperature", "T", "T")
+    create_export(plots, dim, "Heat Source", "Q", "ht.Q")
 
 
-def create_export_elastic(plots, equations: str, args):
-    print("Info    :    Export Elastic")
+def create_export_electric(plots, dim: int, equations: str, args):
+    print("Info    :    Export electric")
 
-    create_export(plots, "Displacement", "disp", "solid.disp")
-    create_export(plots, "Von Mises", "vonmises", "solid.mises")
-    if args.axis:
-        create_export(plots, "Stress", "stress", "solid.srz")
-        if "magnetic" in equations:
-            create_export(
-                plots, "Flaplace", "force laplace", "sqrt((u*mf.Bz)^2+(-u*mf.Br)^2"
-            )
+    create_export(plots, dim, "Electric Potential", "V", "V")
+    create_export(plots, dim, "Electric Field", "E", "ec.normE")
+
+
+def create_export_elastic(plots, dim: int, equations: str, args):
+    print("Info    :    Export elastic")
+
+    create_export(plots, dim, "Displacement", "disp", "solid.disp")
+    create_export(plots, dim, "Von Mises", "vonmises", "solid.mises")
+    if dim == 2:
+        if args.axis:
+            create_export(plots, dim, "Stress", "stress", "solid.srz")
         else:
-            create_export(
-                plots, "Flaplace", "force laplace", "sqrt(F_laplace_0^2+F_laplace_1^2)"
-            )
+            create_export(plots, dim, "Stress", "stress", "solid.sxy")
+        create_export(
+            plots,
+            dim,
+            "Flaplace",
+            "force laplace",
+            "sqrt(F_laplace_0^2+F_laplace_1^2)",
+        )
     else:
-        create_export(plots, "Stress", "stress", "solid.sxy")
-        if "magnetic" in equations:
-            create_export(
-                plots, "Flaplace", "force laplace", "sqrt((-u*mf.By)^2+(-u*mf.Bx)^2)"
-            )
-        else:
-            create_export(
-                plots, "Flaplace", "force laplace", "sqrt(F_laplace_0^2+F_laplace_1^2)"
-            )
+        create_export(plots, dim, "Stress", "stress", "solid.sxyz")
+        create_export(
+            plots,
+            dim,
+            "Flaplace",
+            "force laplace",
+            "sqrt(F_laplace_0^2+F_laplace_1^2+F_laplace_2^2)",
+        )
 
 
-def create_nothing(plots, equations: str, args):
+def create_nothing(plots, dim: int, equations: str, args):
     pass
 
 
-def create_export(plots, name: str, ID: str, expr: str):
-    plot = plots.create("PlotGroup2D", name=name)
+def create_export(plots, dim: int, name: str, ID: str, expr: str):
+    plot = plots.create(f"PlotGroup{dim}D", name=name)
     plot.property("titletype", "manual")
     plot.property("title", name)
     plot.property("showlegendsunit", True)
-    surface = plot.create("Surface", name=ID)
-    surface.property("resolution", "normal")
-    surface.property("expr", expr)
+    if dim == 2:
+        surface = plot.create("Surface", name=ID)
+        surface.property("resolution", "normal")
+        surface.property("expr", expr)
+    else:
+        volume = plot.create("Volume", name=ID)
+        volume.property("resolution", "normal")
+        volume.property("expr", expr)
 
 
-def stat_method(equation, model, data: dict, selection_import: dict, args):
+def stat_method(equation, model, stats: dict, selection_import: dict, dim: int, args):
     ### Statistics
     if args.debug:
         print("Debug   : equation=", equation)
-    stats = data["PostProcess"][equation]["Measures"]["Statistics"]
 
     ### create node groups to sort measures
     if equation == "heat":
@@ -119,15 +130,21 @@ def stat_method(equation, model, data: dict, selection_import: dict, args):
     stat_fct = {"list": create_stat, "str": create_integrate}
 
     for stat in stats:
-        if args.debug:
-            print("Debug   : Stat=", stat)
         typeoftype = type(stats[stat]["type"]).__name__  # usable with stat_fct
         if "%1%" in stat:
-            typeofindex = type(stats[stat]["index1"])
+
+            # print("Info    : Stat=", stat)
+            index = stats[stat]["index1"]
 
             ## case 1 : markers="%1%" and index1==list of markers
-            if typeofindex == list:
-                for mark in stats[stat]["index1"]:
+            if isinstance(index, list):
+
+                for i in track(
+                    range(len(index)),
+                    description=f"Info    :      Stat={stat}",
+                ):
+                    # for mark in index:
+                    mark = index[i]
                     markers = [mark]
                     statname = stat.replace("%1%", mark)
                     stat_fct[typeoftype](
@@ -138,14 +155,18 @@ def stat_method(equation, model, data: dict, selection_import: dict, args):
                         equation,
                         model,
                         selection_import,
+                        dim,
                         args,
                     )
 
             ## case 2 : markers=string+"%1%" and index1="int1:int2"
-            elif typeofindex == str:
+            elif isinstance(index, str):
                 marker = stats[stat]["markers"]
-                index1, index2 = stats[stat]["index1"].split(":")
-                for i in range(int(index1), int(index2)):
+                index1, index2 = index.split(":")
+                for i in track(
+                    range(int(index1), int(index2)),
+                    description=f"Info    :      Stat={stat}",
+                ):
                     markers = [marker.replace("%1%", str(i))]
                     statname = stat.replace("%1%", str(i))
                     stat_fct[typeoftype](
@@ -156,15 +177,51 @@ def stat_method(equation, model, data: dict, selection_import: dict, args):
                         equation,
                         model,
                         selection_import,
+                        dim,
                         args,
                     )
+        elif "%1_1%" in stat:
+            ## case 3 : markers="marker%1_1%" and index1==list of list
+            index = stats[stat]["index1"]
+            marker = stats[stat]["markers"]
+            if isinstance(marker, List):
+                marker = marker[0]
+
+            for i in track(
+                range(len(index)),
+                description=f"Info    :      Stat={stat}",
+            ):
+                id = index[i]
+                statid = stat.replace("%1_1%", str(id[0]))
+                statsid = {statid: stats[stat]}
+                statsid[statid]["expr"] = (
+                    statsid[statid]["expr"]
+                    .replace("%1_1%", str(id[0]))
+                    .replace("%1_2%", str(id[1]))
+                )
+                statsid[statid]["markers"] = [
+                    marker.replace("%1_1%", str(id[0])).replace("%1_2%", str(id[1]))
+                ]
+                stat_fct[typeoftype](
+                    statid,
+                    statsid,
+                    statsid[statid]["markers"],
+                    statid,
+                    equation,
+                    model,
+                    selection_import,
+                    dim,
+                    args,
+                )
         else:
+            if args.debug:
+                print("Info    : Stat=", stat)
             markers = get_markers(stat, stats)
             if stat in markers:
                 markers = None
 
             stat_fct[typeoftype](
-                stat, stats, markers, stat, equation, model, selection_import, args
+                stat, stats, markers, stat, equation, model, selection_import, dim, args
             )
 
 
@@ -176,41 +233,80 @@ def create_integrate(
     equation: str,
     model,
     selection_import: dict,
+    dim: int,
     args,
 ):
-    print(f"Info    :    Statistics {statname}")
+    if args.debug:
+        print(f"Info    :    Statistics {statname}")
     eval = model / "evaluations"
-
-    stat_dict = {
-        "Flux": {
-            "type": "IntLine",
-            "selection": "curve",
-            "expr": stats[stat]["expr"]
-            .replace("Channel%1%", markers[0])
-            .replace("heat_T", "T")
-            .replace("2*pi*x", "2*pi*r")
-            .split(":")[0],
-            "group": "grp3",
-        },
-        "Power": {
-            "type": "IntSurface",
-            "selection": "surface",
-            "expr": "2*pi*r*ht.Q",
-            "group": "grp1",
-        },
-        "MagneticEnergy": {
-            "type": "IntSurface",
-            "selection": "surface",
-            "expr": "2*pi*mf.Aphi*u",
-            "group": None,
-        },
-        "Intensity": {
-            "type": "IntSurface",
-            "selection": "surface",
-            "expr": f"u/N_{markers[0]}",
-            "group": "grp4",
-        },
-    }
+    if dim == 2:
+        si_markers = "surface"
+        si_bound = "curve"
+        stat_dict = {
+            "Flux": {
+                "type": "IntLine",
+                "selection": si_bound,
+                "expr": stats[stat]["expr"]
+                .replace("Channel%1%", markers[0])
+                .replace("Channel%1_1%", markers[0])
+                .replace("heat_T", "T")
+                .replace("2*pi*x", "2*pi*r")
+                .split(":")[0],
+                "group": "grp3",
+            },
+            "Power": {
+                "type": "IntSurface",
+                "selection": si_markers,
+                "expr": "2*pi*r*ht.Q",
+                "group": "grp1",
+            },
+            "MagneticEnergy": {
+                "type": "IntSurface",
+                "selection": si_markers,
+                "expr": "2*pi*mf.Aphi*u",
+                "group": None,
+            },
+            "Intensity": {
+                "type": "IntSurface",
+                "selection": si_markers,
+                "expr": f"u/N_{markers[0]}",
+                "group": "grp4",
+            },
+        }
+    elif dim == 3:
+        si_markers = "volume"
+        si_bound = "surface"
+        stat_dict = {
+            "Flux": {
+                "type": "IntSurface",
+                "selection": si_bound,
+                "expr": stats[stat]["expr"]
+                .replace("Channel%1%", markers[0])
+                .replace("Channel%1_1%", markers[0])
+                .replace("heat_T", "T")
+                .replace("2*pi*x", "2*pi*r")
+                .split(":")[0],
+                "group": "grp3",
+            },
+            "Power": {
+                "type": "IntVolume",
+                "selection": si_markers,
+                "expr": "ht.Q",
+                "group": "grp1",
+            },
+            "MagneticEnergy": {
+                "type": "IntVolume",
+                "selection": si_markers,
+                "expr": "",
+                "group": None,
+            },
+            "Intensity": {
+                "type": "IntSurface",
+                "selection": si_bound,
+                "expr": f"sigma*(Vx*nx+Vy*ny+Vz*nz)",
+                "group": "grp4",
+            },
+        }
 
     for ts in stat_dict:
         if ts in stat:
@@ -240,16 +336,27 @@ def create_stat(
     equation: str,
     model,
     selection_import: dict,
+    dim: int,
     args,
 ):
-    print(f"Info    :    Statistics {statname}")
+    if args.debug:
+        print(f"Info    :    Statistics {statname}")
     eval = model / "evaluations"
+    if dim == 2:
+        si_markers = "surface"
+        type_dict = {
+            "min": "MinSurface",
+            "max": "MaxSurface",
+            "mean": "AvSurface",
+        }
+    elif dim == 3:
+        si_markers = "volume"
+        type_dict = {
+            "min": "MinVolume",
+            "max": "MaxVolume",
+            "mean": "AvVolume",
+        }
 
-    type_dict = {
-        "min": "MinSurface",
-        "max": "MaxSurface",
-        "mean": "AvSurface",
-    }
     if markers:
         typeofstat = stats[stat]["type"]
         for type in typeofstat:
@@ -274,7 +381,7 @@ def create_stat(
                     sur = eval.create(surstat["type"], name=statname + type)
                     sur.select(
                         np.concatenate(
-                            [selection_import["surface"][marker] for marker in markers]
+                            [selection_import[si_markers][marker] for marker in markers]
                         )
                     )
                     sur.property("expr", surstat["expr"])
