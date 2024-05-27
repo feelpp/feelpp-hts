8908ba114235e09e33eef3b5ee8e56e6f447e1a4
Jeremie Muzet <jeremie.muz@gmail.com>
2024-05-27 13:40:44 +0200

    small fixes (docs skip)

--- src/cases/_python_magnetComsol-pyMPH/parameters.py
+++ src/cases/_python_magnetComsol-pyMPH/parameters.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 from typing import List
-from methods import get_markers
+from methods import get_markers, feel_to_comsol_symbols
 from paraview.simple import *
 
 
@@ -13,7 +13,7 @@ def parameters_and_functions(
     basedir: str,
     I: float = 31000.0,
     mdata: dict = None,
-    time: List[float] = None,
+    times: List[float] = None,
     debug: bool = False,
 ) -> dict:
     params_unknowns = {}
@@ -39,10 +39,10 @@ def parameters_and_functions(
             model.parameter(p, str(feel_parameters[p]))
 
     # Time dependent parameters
-    if time:
-        model.parameter("time_initial", time[0])
-        model.parameter("time_step", time[1])
-        model.parameter("time_final", time[2])
+    if times:
+        model.parameter("time_initial", times[0])
+        model.parameter("time_step", times[1])
+        model.parameter("time_final", times[2])
 
     # Chosen I
     if mdata:
@@ -68,19 +68,19 @@ def material_variables(
     elif dim == 3:
         si_markers = "volume"
     for mat in materials:
-        model.java.component("component").variable().create("var_" + mat)
-        model.java.component("component").variable("var_" + mat).label(mat)
+        model.java.component("component").variable().create(f"var_{mat}")
+        model.java.component("component").variable(f"var_{mat}").label(mat)
         # model.java.variable("var_"+mat).model('component')
 
         markers = get_markers(mat, materials)
 
         # print(model.java.variable("var_"+mat).selection())
-        model.java.component("component").variable("var_" + mat).selection().geom(2)
-        model.java.component("component").variable("var_" + mat).selection().set(
+        model.java.component("component").variable(f"var_{mat}").selection().geom(dim)
+        model.java.component("component").variable(f"var_{mat}").selection().set(
             np.concatenate([selection_import[si_markers][ma] for ma in markers])
         )
         # for each materials in the json, creating the law and material in comsol
-        print('Info    : Loading Parameters from material "' + mat + '"...')
+        print(f'Info    : Loading Parameters from material "{mat}"...')
 
         for p in materials[
             mat
@@ -89,36 +89,37 @@ def material_variables(
                 if type(materials[mat][p]) == str:
                     var = materials[mat][p].split(":")[0]
 
+                    # if var is a vector, separate it in its parts
                     if "{" in var:
                         var = var[var.find("{") + 1 : var.find("}")].split(",")
                         for i in range(len(var)):
-                            for word in unknowns:
-                                var[i] = var[i].replace(word, unknowns[word])
+                            var[i] = feel_to_comsol_symbols(var[i], unknowns)
                             model.java.component("component").variable(
-                                "var_" + mat
+                                f"var_{mat}"
                             ).set(f"{p}_{i}", var[i])
+                    # if var is scalar/ expr
                     else:
-                        for word in unknowns:
-                            var = var.replace(word, unknowns[word])
-                        model.java.component("component").variable("var_" + mat).set(
+                        var = feel_to_comsol_symbols(var, unknowns)
+                        model.java.component("component").variable(f"var_{mat}").set(
                             p, var
                         )
 
                 else:
-                    model.java.component("component").variable("var_" + mat).set(
+                    model.java.component("component").variable(f"var_{mat}").set(
                         p, str(materials[mat][p])
                     )
 
+            # if electric conductiviy/resitivity create electric resistivity/conductivy
             if p == "sigma":
-                model.java.component("component").variable("var_" + mat).set(
+                model.java.component("component").variable(f"var_{mat}").set(
                     "rho", "1/sigma"
                 )
             elif p == "rho":
-                model.java.component("component").variable("var_" + mat).set(
+                model.java.component("component").variable(f"var_{mat}").set(
                     "sigma", "1/rho"
                 )
 
-        # print("Info    : Done loading Parameters from material \""+mat+"\"...")
+        # print(f'Info    : Done loading Parameters from material "{mat}"...')
 
 
 def import_fields(
@@ -130,9 +131,14 @@ def import_fields(
     axis: bool,
     debug: bool = False,
 ):
+    # instead of importing h5, load Export.case and creat a csv, then load the parts of the csv as interpolations
     fields_unknowns = {}
     for f in Fields:
-        file = Fields[f]["filename"].replace("$cfgdir", basedir).replace(".h5", ".csv")
+        file = (
+            Fields[f]["filename"]
+            .replace("$cfgdir", f"{basedir}/Comsol_res")
+            .replace(".h5", ".csv")
+        )
 
         if "v" in Fields[f]["basis"]:
             paraview_export(f, file, feelpp_directory)
@@ -146,9 +152,11 @@ def import_fields(
                         inplace=True,
                     )
                     df.to_csv(filei, index=False)
+
+                # feelpp symbols need to be replaced with corresponding comsol symbols
                 if axis:
                     fields_unknowns[f"meshes_cfpdes_fields_{f}_{i}"] = (
-                        f"meshes_cfpdes_fields_{f}_{i}(x,z,0)"
+                        f"meshes_cfpdes_fields_{f}_{i}(r,z,0)"
                     )
                 elif dim == 2:
                     fields_unknowns[f"meshes_cfpdes_fields_{f}_{i}"] = (
@@ -175,7 +183,7 @@ def import_fields(
 
             if axis:
                 fields_unknowns[f"meshes_cfpdes_fields_{f}"] = (
-                    f"meshes_cfpdes_fields_{f}(x,z,0)"
+                    f"meshes_cfpdes_fields_{f}(r,z,0)"
                 )
             elif dim == 2:
                 fields_unknowns[f"meshes_cfpdes_fields_{f}"] = (
@@ -196,10 +204,9 @@ def import_fields(
 
 
 def paraview_export(field: str, file: str, feelpp_directory: str):
+    # export csv in paraview for comsol interpolation
     print(
-        'Info    : Loading Interpolation from field "meshes_cfpdes_fields_'
-        + field
-        + '"...'
+        f'Info    : Loading Interpolation from field "meshes_cfpdes_fields_{field}"...'
     )
     exportcase = EnSightReader(
         registrationName="Export.case",
